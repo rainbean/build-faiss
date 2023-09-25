7136329b31a948c6ff5a3111a07675e2988701f0
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2023-09-25 16:13:08 +0800

    build and pack faiss and mkl in single package

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,12 +14,14 @@ fi
 
 MKL_PATH=$PWD/vcpkg/installed/x64-linux/lib/intel64
 MKL_LIBRARIES="-Wl,--start-group;${MKL_PATH}/libmkl_intel_lp64.a;${MKL_PATH}/libmkl_gnu_thread.a;${MKL_PATH}/libmkl_core.a;-Wl,--end-group -ldl"
+DIST_PATH=dist
 
 # configure build and compile
 echo "::group::Configure CMake and Build ..."
-rm -fr build
+rm -fr build dist
 cmake -Bbuild \
     -Wno-dev \
+    -DCMAKE_INSTALL_PREFIX="${DIST_PATH}" \
     -DFAISS_ENABLE_PYTHON=OFF \
     -DFAISS_OPT_LEVEL=avx2 \
     -DFAISS_ENABLE_GPU=OFF \
@@ -27,9 +29,18 @@ cmake -Bbuild \
     -DMKL_LIBRARIES="${MKL_LIBRARIES}" \
     faiss
 
-make -C build -j4 faiss
-make -C build -j4 demo_ivfpq_indexing
+make -C build -j4 install
 echo "::endgroup::"
 
-ls -lah build/faiss/libfaiss.a
-ls -lah build/demos/demo_ivfpq_indexing
+# copy artifacts and change config
+cp $MKL_PATH/libmkl_intel_lp64.a $DIST_PATH/lib
+cp $MKL_PATH/libmkl_gnu_thread.a $DIST_PATH/lib
+cp $MKL_PATH/libmkl_core.a $DIST_PATH/lib
+sed -i "s@$MKL_PATH@\${_IMPORT_PREFIX}/lib@g" $DIST_PATH/share/faiss/faiss-targets.cmake
+
+# pack binary
+echo "::group::Pack artifacts ..."
+TARGET=${1:-'faiss-linux.tar.zst'}
+tar "-I zstd -3 -T4 --long=27" -cf $TARGET \
+    -C $DIST_PATH $(cd $DIST_PATH; echo *)
+echo "::endgroup::"
